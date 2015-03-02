2a01451e9aa1b7c359116076245cdd1e0c76343c
Larry Pace <pace.larry@gmail.com>
2015-03-02 15:05:52 -0500

    Making changes to descriptive stats lesson.

--- Getting_Started/Descriptive_Statistics/initLesson.R
+++ Getting_Started/Descriptive_Statistics/initLesson.R
@@ -2,3 +2,4 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+assign("cars", openintro::cars, envir = globalenv())
